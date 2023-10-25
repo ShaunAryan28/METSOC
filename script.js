b55ebbe1d26bddfa2eb117ebe4d594b46a030cee
ShaunAryan28 <123452148+ShaunAryan28@users.noreply.github.com>
2023-10-25 18:11:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
     const element = document.getElementById('embed-iframe');
     const options = {
-    //   width: '600rem',
-    //   height: '500rem',
+       width: '600rem',
+      height: '500rem',
 
     uri: 'https://open.spotify.com/playlist/3e8kWcCuI1IpIFU4SxaFx6?si=43a15e480ea54c82'
     };
